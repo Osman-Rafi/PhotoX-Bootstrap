4fcce7fc20b8171877d30d68b7f2c9dc981a8c8a
Osman Rafi <rafi.ogchy@gmail.com>
2019-06-06 19:17:48 +0600

    Update

--- script/script.js
+++ script/script.js
@@ -9,12 +9,25 @@ $(document).ready(function () {
             $('.filter').show(300);
         } else {
             $('.filter').not('.' + value).hide();
-            $('.filter').filter('.'+ value).show();
+            $('.filter').filter('.' + value).show();
         }
     });
 
     $('.gallery-list-item').click(function () {
         $(this).addClass('active-item')
-    })
+    });
 
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if (position >= 4800) {
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        }
+        else {
+            $('.card-1').removeClass('moveFromLeft');
+            $('.card-2').removeClass('moveFromBottom');
+            $('.card-3').removeClass('moveFromRight');
+        }
+    });
 });
